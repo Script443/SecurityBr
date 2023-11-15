26004e3a9ac88e6d080f6ab35fb76193ad33379d
Leonardo Lima <79790014+Script443@users.noreply.github.com>
2023-11-15 14:32:19 -0300

    Delete my_test.php

--- my_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$s = fopen(__DIR__ . "/font.font", "w");
-// header without character data
-fwrite($s, "\x01\x00\x00\x00\x20\x00\x00\x00\x08\x00\x00\x00\x08\x00\x00\x00");
-fclose($s);
-$font = imageloadfont(__DIR__ . "/font.font");
-$im = imagecreate(10, 10);
-imagechar($im, $font, 0, 0, " ", imagecolorallocate($im, 255, 255, 255));
