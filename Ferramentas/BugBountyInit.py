74350073a8eb8e705f9343ea324c0fba2e2075c6
Leonardo Lima <79790014+Script443@users.noreply.github.com>
2023-11-16 16:19:03 -0300

    Update BugBountyInit.py

--- Ferramentas/BugBountyInit.py
+++ Ferramentas/BugBountyInit.py
@@ -1,8 +1,8 @@
 import subprocess
+import os
 
 def install_tools():
     try:
-        subprocess.run(["cd", "home/"], check=True)
         subprocess.run(["apt", "update"], check=True)
         subprocess.run(["apt", "install", "nano", "-y"], check=True)
         subprocess.run(["apt", "install", "pip", "-y"], check=True)
@@ -11,10 +11,10 @@ def install_tools():
         subprocess.run(["wget", "https://go.dev/dl/go1.21.4.linux-amd64.tar.gz"], check=True)
         subprocess.run(["rm", "-rf", "/usr/local/go"], check=True)
         subprocess.run(["tar", "-C", "/usr/local", "-xzf", "go1.21.4.linux-amd64.tar.gz"], check=True)
-        subprocess.run(["export", "PATH=$PATH:/usr/local/go/bin"], check=True)
+        os.environ['PATH'] += ':/usr/local/go/bin'
         subprocess.run(["go", "install", "-v", "github.com/tomnomnom/anew@latest"], check=True)
         subprocess.run(["cp", "/root/go/bin/anew", "/usr/bin/"], check=True)
-        subprocess.run(["pip", "install", "\'httpx[cli]\'"], check=True)
+        subprocess.run(["pip", "install", "httpx[cli]"], check=True)
         print("All tools installed successfully!")
     except subprocess.CalledProcessError as e:
         print(f"Error during installation: {e}")
